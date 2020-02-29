23980c9c0326e89d703769ce30c52e67da361553
Tristan Gruener <tristan.gruener@gmail.com>
2020-02-28 18:17:34 -0800

    save

--- src/components/Tabs/Tab.js
+++ src/components/Tabs/Tab.js
@@ -3,30 +3,23 @@ import React from 'react'
 function Tab({ tabActive, tab, handleClick }) {
 
   const firstString = tab.split('-')[0]
-  let secondString = tab.split('-')[1]
+  const secondString = tab.split('-')[1]
 
-  // if(secondString === undefined) {
-  //   secondString = 'boobs'
-  // }
-
-  console.log(firstString, secondString)
-
-  const sanitizedFirstString = firstString.split('').map((letter, i) => (
-    i === 0 ? letter.toUpperCase() : letter
-  )).join('')
-
-  // console.log(sanitizedFirstString)
+  const sanitizedFirstString = Sanitize(firstString)
 
   let sanitizedSecondString = ''
+
   if(secondString !== undefined) {
-    sanitizedSecondString = secondString.split('').map((letter, i) => (
-      i === 0 ? letter.toUpperCase() : letter
-    )).join('')
+    sanitizedSecondString = Sanitize(secondString)
   } else {
     sanitizedSecondString = ''
   }
 
-  console.log(sanitizedFirstString + ' ' + sanitizedSecondString)
+  function Sanitize(word) {
+    return word.split('').map((letter, i) => (
+      i === 0 ? letter.toUpperCase() : letter
+    )).join('')
+  }
 
   return (
     <li className={tabActive === tab && 'is-active'} onClick={() => handleClick(tab)}>
