7b8dfad2b658906833a5adbf3e5bbc91b05d4c0b
Tristan Gruener <tgruener@nordicnaturals.com>
2020-03-01 15:34:29 -0800

    basic search html

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,8 +14,11 @@ function Home() {
     })
 
   return (
-    <div className="home">
-      <p>homepage...</p>
+    <div className="home content" style={{paddingTop: '88px', paddingLeft: '88px', paddingRight: '144px'}}>
+      <center>
+        <h3>Nordic FAQs Search</h3>
+        <input className="input is-rounded" type="text" placeholder="Prodcut / Bulk / Raw Material / Nordic Standard / FAQs" />
+      </center>
     </div>
   )
 }
