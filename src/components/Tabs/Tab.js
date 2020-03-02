6a7d23aa0e9a1324de8b588df94e8389fd8208cf
Tristan Gruener <tgruener@nordicnaturals.com>
2020-03-02 14:48:10 -0800

    more cleanup

--- src/components/Tabs/Tab.js
+++ src/components/Tabs/Tab.js
@@ -2,26 +2,6 @@ import React from 'react'
 
 function Tab({ tabActive, tab, handleClick }) {
 
-  // const tabs = tab.split('-')
-  //
-  //
-  //
-  //
-  //
-  // const firstString = tab.split('-')[0]
-  // const secondString = tab.split('-')[1]
-  //
-  // const sanitizedFirstString = Sanitize(firstString)
-  //
-  // let sanitizedSecondString = ''
-  //
-  // if(secondString !== undefined) {
-  //   sanitizedSecondString = Sanitize(secondString)
-  // } else {
-  //   sanitizedSecondString = ''
-  // }
-  //
-
   //return word with first letter capitalized
   function Sanitize(word) {
     return word.split('').map((letter, i) => (
@@ -30,10 +10,6 @@ function Tab({ tabActive, tab, handleClick }) {
   }
 
   return (
-    // <li className={tabActive === tab && 'is-active'} onClick={() => handleClick(tab)}>
-    //   <a><span>{sanitizedFirstString + ' ' + sanitizedSecondString}</span></a>
-    // </li>
-
     <li className={tabActive === tab ? 'is-active' : ''} onClick={() => handleClick(tab)}>
       <a><span>{tab}</span></a>
     </li>
